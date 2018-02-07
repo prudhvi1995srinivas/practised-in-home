27f575c52ee44d39d4305d103b109cda39e8d42a
prudhvi <pr313415@dal.ca>
2018-02-07 12:05:37 +0530

    modified

--- maps.py
+++ maps.py
@@ -5,8 +5,14 @@ import requests
 
 
 api_address = 'http://maps.googleapis.com/maps/api/geocode/json?'
-address = 'yhz'
 
-url = api_address + urllib.parse.urlencode({'address':address}) 
-json_data = requests.get(url).json() 
-print(json_data)
+while True:
+    address = input('address: ')
+    url = api_address + urllib.parse.urlencode({'address':address})
+    print(url)
+    json_data = requests.get(url).json() 
+    print(json_data)
+
+    json_status = json_data['status']
+    print('API Status:' + json_status)
+ 
