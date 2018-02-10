4d74a46bb4e5abcd0a94435c7a1086de90d1fb6c
prudhvi <pr313415@dal.ca>
2018-02-11 01:26:57 +0530

    using these code you can send messages to the phone

--- cred.py
+++ cred.py
@@ -1,4 +1,4 @@
-account_sid = 'ACbb63a12a760c6c66b05f7b9ce6cd6e80'
-auth_token = '25593ecd75f9b4f29c11549265d8a80a'
+account_sid = 'generate key from twilio'
+auth_token = 'auth key from twilio'
 my_cell = 'your number here'
 my_twilio = 'your twilio number here'
